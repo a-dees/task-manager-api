e2060603fa364695145ea0f6ffbf8549ff3ae2bd
Alaura <adees40@wgu.edu>
2026-05-09 17:45:04 -0400

    Added task controller

---
+++ src/main/java/com/example/taskmanager/controller/TaskController.java
@@ -0,0 +1,35 @@
+package com.example.taskmanager.controller;
+import com.example.taskmanager.entity.Task;
+import com.example.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+}
